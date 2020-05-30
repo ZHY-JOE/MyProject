367f93fc415b292162d1f9372bf390debe230c59
ZHY-JOE <337347889@qq.com>
2020-05-30 23:36:43 +0800

    期中作业

---
+++ NotePad/app/src/main/java/com/example/android/notepad/NoteSearch.java
@@ -0,0 +1,92 @@
+package com.example.android.notepad;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SimpleCursorAdapter;
+
+public class NoteSearch extends Activity implements SearchView.OnQueryTextListener{
+    ListView listView;
+    SQLiteDatabase sqLiteDatabase;
+    /**
+     * The columns needed by the cursor adapter
+     */
+    private static final String[] PROJECTION = new String[]{
+            NotePad.Notes._ID, // 0
+            NotePad.Notes.COLUMN_NAME_TITLE, // 1
+            NotePad.Notes.COLUMN_NAME_MODIFICATION_DATE//时间
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note_search);
+        SearchView searchView = findViewById(R.id.search_view);
+        Intent intent = getIntent();
+        if (intent.getData() == null) {
+            intent.setData(NotePad.Notes.CONTENT_URI);
+        }
+        listView = findViewById(R.id.list_view);
+        sqLiteDatabase = new NotePadProvider.DatabaseHelper(this).getReadableDatabase();
+        searchView.setOnQueryTextListener(this);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String string) {
+        String selection1 = NotePad.Notes.COLUMN_NAME_TITLE+" like ? or "+NotePad.Notes.COLUMN_NAME_NOTE+" like ?";
+        String[] selection2 = {"%"+string+"%","%"+string+"%"};
+        /*
+         * Performs the query. If no problems occur trying to read the database, then a Cursor
+         * object is returned; otherwise, the cursor variable contains null. If no records were
+         * selected, then the Cursor object is empty, and Cursor.getCount() returns 0.
+         */
+        Cursor cursor = sqLiteDatabase.query(
+                NotePad.Notes.TABLE_NAME,
+                PROJECTION, // The columns to return from the query
+                selection1, // The columns for the where clause
+                selection2, // The values for the where clause
+                null,          // don't group the rows
+                null,          // don't filter by row groups
+                NotePad.Notes.DEFAULT_SORT_ORDER // The sort order
+        );
+        /*
+         * The following two arrays create a "map" between columns in the cursor and view IDs
+         * for items in the ListView. Each element in the dataColumns array represents
+         * a column name; each element in the viewID array represents the ID of a View.
+         * The SimpleCursorAdapter maps them in ascending order to determine where each column
+         * value will appear in the ListView.
+         */
+        // The names of the cursor columns to display in the view, initialized to the title column
+        String[] dataColumns = {
+                NotePad.Notes.COLUMN_NAME_TITLE,
+                NotePad.Notes.COLUMN_NAME_MODIFICATION_DATE
+        } ;
+        // The view IDs that will display the cursor columns, initialized to the TextView in
+        // noteslist_item.xml
+        int[] viewIDs = {
+                android.R.id.text1,
+                R.id.text1_date
+        };
+        // Creates the backing adapter for the ListView.
+        SimpleCursorAdapter adapter
+                = new SimpleCursorAdapter(
+                this,                             // The Context for the ListView
+                R.layout.noteslist_item,         // Points to the XML for a list item
+                cursor,                           // The cursor to get items from
+                dataColumns,
+                viewIDs
+        );
+        // Sets the ListView's adapter to be the cursor adapter that was just created.
+        listView.setAdapter(adapter);
+        return true;
+     }
+}
+
